fb69d3d51574442f0b4342940a9cc50ded01a032
Kaaim-byte <kauancarabreu2016@gmail.com>
2025-10-30 22:45:22 +0000

    Adicao da Densidade Populacional e PIB per capita as cartas do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,6 +13,8 @@ int Populacao;
 float area;
 float PIB;
 int Numero_de_pontos_turisticos;
+float Densidade_Populacional;
+float PIB_per_capita;
   // Fim do espaco para criacao de variaveis
 
   // Inicio da entradas de dados para primeira e segunda carta
@@ -33,8 +35,13 @@ scanf(" %f", &PIB);
 printf("Quantos pontos turisticos existem na sua cidade?\n");
 scanf(" %i", &Numero_de_pontos_turisticos);
   // Fim da entrada de dados para carta 1
-  // Exibicao dos dados da carta 1
+  // Inicio dos calculos adicionais para carta 1
 
+Densidade_Populacional = (Populacao / area);
+PIB_per_capita = (PIB / Populacao);
+
+  // Fim dos calculos adicionais para carta 1
+  // Exibicao dos dados da carta 1
 printf("Segue os dados da Carta 1:\n");
 
 
@@ -45,7 +52,10 @@ printf("Populacao da cidade e: %i\n", Populacao);
 printf("Area da cidade e: %f\n", area);
 printf("O PIB da cidade e: %f\n", PIB);
 printf("Numero de pontos turisticos: %i\n", Numero_de_pontos_turisticos);
-  // Fim da exibicao dos dados da carta 1
+printf("A Densidade populacional da sua cidade e: %f\n", Densidade_Populacional);
+printf("O PIB per capita da sua cidade e: %f\n", PIB_per_capita);  
+
+// Fim da exibicao dos dados da carta 1
   // Carta 2
 
 printf("Digite abaixo os dados da sua segunda carta\n");
@@ -66,6 +76,11 @@ printf("Quantos pontos turisticos existem na sua cidade?\n");
 scanf(" %i", &Numero_de_pontos_turisticos);
 
   // Fim da entrada de dados para segunda carta
+  // Inicio dos calculos adicionais da segunda carta
+Densidade_Populacional = (Populacao / area);
+PIB_per_capita = (PIB / Populacao);
+ 
+  // Fim dos calculos adicionais da segunda carta
   // Exibicao dos dados da carta 2
 
 printf("Seguem os dados da sua segunda carta\n");
@@ -77,6 +92,9 @@ printf("Populacao da cidade: %i\n", Populacao);
 printf("Area da cidade e: %f\n", area);
 printf("O PIB da cidade e: %f\n", PIB);
 printf("Numero de pontos turisticos: %i\n", Numero_de_pontos_turisticos);
+printf("A Densidade populacional da sua cidade e: %f\n", Densidade_Populacional);
+printf("O PIB per capita da sua cidade e: %f\n", PIB_per_capita);  
+
   // Fim da exibicao dos dados da carta 2
 // fim da entrada de dados para primeira e segunda carta
 
